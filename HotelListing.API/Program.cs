cddafa55ebd32c1c127981cbc29c41775ca12bfb
darren <dp161074@hotmail.com>
2022-06-23 06:50:18 +0100

    Classes and Db setup added

--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -1,3 +1,5 @@
+using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<HotelListingDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("HotelListingConnectionString"));
+});
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", 
